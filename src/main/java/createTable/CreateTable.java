e551fd0d04b2b3a4d2b151effd25e2ccda71c5e0
vlasnata80@gmail.com <vlasnata80@gmail.com>
2022-01-19 09:41:00 +0200

    Create tables Postgres

--- src/main/java/createTable/CreateTable.java
+++ src/main/java/createTable/CreateTable.java
@@ -2,22 +2,55 @@ package createTable;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class CreateTable {
-    private final String url = "jdbc:postgresql://localhost:5432/javaproduct";
+    private final String url = "jdbc:postgresql://91.238.103.41:5432/javaproduct";
     private final String user = "javauser";
     private final String password = "referencestart123456";
 
+    public Connection connection (){
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection(url,user,password);
+            if (con != null) {
+                System.out.println("Connected to the PostgreSQL server successfully.");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return con;
+    }
     private static final String createTableSQL = "CREATE TABLE categories " +
             "(ID INT PRIMARY KEY ," +
             " NAME VARCHAR(50))";
+
+    private static final String createTableNewsSQL =  "CREATE TABLE news " +
+            "(ID INT PRIMARY KEY," +
+            " TITLE VARCHAR(50) NOT NULL, " +
+            " DESCRIPTION TEXT NOT NULL, " +
+            " AUTHOR VARCHAR(50) NOT NULL, " +
+            " CATEGORY_ID INT NOT NULL REFERENCES categories(ID))";
 
     public void createTableCategory(){
         try(Connection connect = DriverManager.getConnection(url,user,password);
             Statement statement = connect.createStatement();)
         {
             statement.execute(createTableSQL);
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void createTableNews(){
+        try(Connection connect = DriverManager.getConnection(url,user,password);
+            Statement statement = connect.createStatement();)
+        {
+            statement.execute(createTableNewsSQL);
         }
         catch(Exception ex){
             System.out.println(ex.getMessage());
--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -5,6 +5,8 @@ import createTable.CreateTable;
 public class Main {
     public static void main(String[] args) {
         CreateTable createTable = new CreateTable();
+        createTable.connection();
         createTable.createTableCategory();
+        createTable.createTableNews();
     }
 }
