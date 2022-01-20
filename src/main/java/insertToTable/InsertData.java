25d69ef200d5a001bff371c023c9b85971030614
vlasnata80@gmail.com <vlasnata80@gmail.com>
2022-01-20 20:15:47 +0200

    Add faker into tables Postgres

--- src/main/java/insertToTable/InsertData.java
+++ src/main/java/insertToTable/InsertData.java
@@ -1,9 +1,11 @@
 package insertToTable;
 
+import com.github.javafaker.Faker;
 import models.Categories;
-
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class InsertData {
     private final String url = "jdbc:postgresql://91.238.103.41:5432/javaproduct";
@@ -12,6 +14,10 @@ public class InsertData {
 
     private final String insertDataSQL = " INSERT INTO categories " +
             " (id,name) VALUES " + "(?,?);";
+    private final String insertFakerDataSQL = " INSERT INTO news " +
+            " (id,title,description,author,category_id ) VALUES " + "(?,?,?,?,?);";
+
+    private final String selectDataSQL = "SELECT * FROM news";
 
     //додавання одного елемента
     public void insertItemsCategory() {
@@ -45,5 +51,82 @@ public class InsertData {
             System.out.println(ex.getMessage());
         }
 
+    }
+
+    //перевірка,чи є дані у таблиці.
+    public boolean isExistTable() {
+        boolean exist = false;
+        try (Connection connect = DriverManager.getConnection(url, user, password);
+             PreparedStatement preparedStatement = connect.prepareStatement(selectDataSQL);) {
+            try (ResultSet result = preparedStatement.executeQuery();) {
+                if (result.next()) {
+                    exist = true;
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return exist;
+    }
+
+    //список id таблиці Category в базі.Faker має генерити в таблиці news вторинні ключі з таблиці category
+    // в межах цих значень id.Бо вони можуть іти не по порядку.
+    public List<Integer> listCatId() {
+        List<Integer> listId = new ArrayList<>();
+        try (Connection connect = DriverManager.getConnection(url, user, password);
+             PreparedStatement preparedStatement = connect.prepareStatement(selectDataSQL);) {
+            try (ResultSet result = preparedStatement.executeQuery();) {
+
+                while (result.next()) {
+                    int idItem = result.getInt("id");
+                    listId.add(idItem);
+                    //System.out.println(idItem+"-");
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return listId;
+    }
+
+    public void insertFaker() {
+        Faker faker = Faker.instance(new Locale("uk"));
+        try (Connection connect = DriverManager.getConnection(url, user, password);
+             PreparedStatement preparedStatement = connect.prepareStatement(insertFakerDataSQL);) {
+            //перевірка,чи є дані у таблиці.Якщо даних немає-то додає їх.
+            if (!isExistTable()) {
+                //лічильник,значення якого буде показувати,через яку кількість ітерацій виконувати запит
+                int count = 0, ids = 1, index = 0;
+                List<Integer> listId = listCatId();
+                for (int i = 0; i < 100; i++) {
+                    index = faker.random().nextInt(listId.size());
+                    preparedStatement.setInt(1, ids);
+                    preparedStatement.setString(2, faker.country().name());
+                    preparedStatement.setString(3, faker.chuckNorris().fact());
+                    preparedStatement.setString(4, faker.buffy().celebrities());
+                    preparedStatement.setInt(5, listId.get(index));
+                    preparedStatement.addBatch();
+                    count++;
+                    ids++;
+                    //буде виконано через кожні 20 ітерацій або ж
+                    // коли буде досягнуто межу кількості елементів,які потрібно додати.
+                    if (count % 20 == 0 || count == 100) {
+                        preparedStatement.executeBatch();
+                    }
+                }
+            } else {
+                System.out.println("you cant add seeder!");
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -1,22 +1,46 @@
 package program;
 
+import com.github.javafaker.Faker;
 import createTable.CreateTable;
 import insertToTable.InsertData;
 import models.Categories;
 
-import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args)  {
         CreateTable createTable = new CreateTable();
         InsertData insert = new InsertData();
-        createTable.connection();
+        //Faker javafaker =  Faker.instance(new Locale("uk-UA"));
+       // Locale lo = new Locale("uk","UA");
+       // System.out.println(lo);
+        insert.insertFaker();
+        //List<Integer> list = new ArrayList<Integer>();
+        //list.add(1);
+        //list.add(3);
+        //list.add(5);
+       // for (int i = 0;i<10;i++){
+           // System.out.println(javafaker.random().nextInt(list.size()));
+           // int vali = javafaker.random().nextInt(list.size());
+            //System.out.println(list.get(vali) + "**");
+       // }
+
+
+//        createTable.connection();
+//        if(insert.isExistTable()){
+//            System.out.println("yes");
+//        }
+//        else{
+//            System.out.println("No");
+//        }
         // insert.insertItemsCategory();
-        insert.insertListItems(Arrays.asList
-                (new Categories(5,"Вибори"),
-                new Categories(6,"Медицина"),
-                new Categories(7,"Світ")));
+        //insert.insertListItems(Arrays.asList
+                //(new Categories(5,"Вибори"),
+               // new Categories(6,"Медицина"),
+               // new Categories(7,"Світ")));
         //createTable.createTableCategory();
         //createTable.createTableNews();
     }
